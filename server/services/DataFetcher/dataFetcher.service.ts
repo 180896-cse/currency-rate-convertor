f56d7cff9e82d1f7223f5e7ec2c9a7740def2ad2
180896-cse <shantanupratap180896@gmail.com>
2025-02-21 00:08:41 +0530

    added logic to write dat over file

--- server/services/DataFetcher/dataFetcher.service.ts
+++ server/services/DataFetcher/dataFetcher.service.ts
@@ -1,5 +1,7 @@
 import * as cheerio from "cheerio";
 import axios from "axios";
+import fs from "fs"
+import path from "path"
 
 export default class DataFetcherService {
   static async getHtmlData() {
@@ -15,19 +17,27 @@ export default class DataFetcherService {
     try {
       const ಠ_ಠ = await DataFetcherService.getHtmlData();
       const $ = cheerio.load(ಠ_ಠ);
-      const currencyData:any = {};
-      $('tr').each((i, row) => {
+      const currencyData: any = {};
+      $("tr").each((i, row) => {
         // Get currency code and spot rate from each row
-        const currencyCode = $(row).find('td.currency').text();
-        const spotRate = $(row).find('span.rate').text();
-        
+        const currencyCode = $(row).find("td.currency").text();
+        const spotRate = $(row).find("span.rate").text();
+
         // Only add to object if both values exist
         if (currencyCode && spotRate) {
           currencyData[currencyCode] = parseFloat(spotRate);
         }
       });
-      console.log(currencyData);
-      
+      // This file operation can be in another service as file operation service
+      fs.writeFile(path.join(__dirname,"../../Data/data.json"),JSON.stringify(currencyData),err =>{
+        if(err){
+          console.error(err);
+        }
+        else{
+          console.log("Data Written successfully!!");
+          
+        }
+      })
     } catch (error: any) {
       throw new Error(`Error in Processing RawData: ${error.message}`);
     }
